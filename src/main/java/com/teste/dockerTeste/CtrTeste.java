b3947de1faee16a5112833e998a5fec09db71a5a
Douglas Dias <douglasddrdias@gmail.com>
2024-08-28 22:53:22 -0300

    Alteração para identificar atualização do container.

--- src/main/java/com/teste/dockerTeste/CtrTeste.java
+++ src/main/java/com/teste/dockerTeste/CtrTeste.java
@@ -49,6 +49,6 @@ public class CtrTeste {
                 ex.printStackTrace();
             }
         }
-        return ResponseEntity.ok("Tudo funcionando !!! - alterado 22:23 git" + minhaVariavel);
+        return ResponseEntity.ok("Tudo funcionando !!! - alterado 22:47 direto" + minhaVariavel);
     }
 }
